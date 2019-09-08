b665f16ccef0c0ba1e91963c39d9fac0e7eeace4
kyle <kdchin@andrew.cmu.edu>
2019-09-08 00:40:51 -0400

    added cors in

--- hello.py
+++ hello.py
@@ -6,7 +6,9 @@ import scipy
 import psycopg2
 import pandas
 
+# @cross_origin(origins="https://webuild-ai.herokuapp.com/")
 @app.route('/marco', methods=['GET'])
+@cross_origin()
 def marco():
     return 'polo'
 
